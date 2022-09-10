135eb3c67de3b69ba25069713369e2a90be3d40e
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-10 15:08:35 -0700

    prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - print all single digit base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	/* your code goes there */
+	for (n = 48;  n < 58; n++)
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+	putchar('\n');
+	return (0);
+}
