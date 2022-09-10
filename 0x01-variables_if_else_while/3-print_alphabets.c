7a11364471ddcb317c1aed1501deba08571b8b9e
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-09 22:57:48 -0700

    print lower and uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - list the alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c, C;
+
+	/* your code goes there */
+	c = 'a';
+	C = 'A';
+	do {
+		putchar(c);
+		c++;
+	} while (c != ('z' + 1));
+	do {
+                putchar(C);
+                c++;
+        } while (C != ('Z' + 1));
+        putchar('\n');
+
+	return (0);
+}
