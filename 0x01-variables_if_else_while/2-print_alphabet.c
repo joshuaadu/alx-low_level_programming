6b9f93daec626977769d09cbf1fab1dd89cd9838
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-09 22:45:47 -0700

    list alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - list the alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	/* your code goes there */
+	c = 'a';
+	do {
+		putchar(c);
+		c++;
+	} while (c != 'z');
+	putchar(c);
+	return (0);
+}
