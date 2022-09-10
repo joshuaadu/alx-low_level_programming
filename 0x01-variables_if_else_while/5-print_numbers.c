e0b1438f37f4188b90b158f886796c752767ebec
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-10 12:19:24 -0700

    print numbers 0 - 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - print all single digit base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	/* your code goes there */
+	for (n = 0;  n < 10; n++)
+		putchar(n)
+	putchar('\n');
+	return (0);
+}
