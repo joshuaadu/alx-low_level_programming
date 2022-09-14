6aec1dbf7a103687256ad77626223df312a34c5e
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-13 22:55:26 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 
 
 /**
- * main - check the code
  *
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
 	char c = 'a';
-	while(c < 123)
+	while (c < 123)
 	{
 		_putchar(c);
 		++c;
