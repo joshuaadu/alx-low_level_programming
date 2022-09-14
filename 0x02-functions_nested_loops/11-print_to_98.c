b76cdd46b7e3da8385efd15faa1550e10bb9a70d
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-14 13:55:20 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,17 @@
 void print_to_98(int n)
 {
 	if (n > 98) {
-		while (n > 97)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			--n;
 		}
 	} else {
-		while (n < 99)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			++n;
 		}
 	}
-	printf("\n");
+	printf("%d\n", n);
 }
