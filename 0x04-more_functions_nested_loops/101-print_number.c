7dc5b886126d7998b95993705512dc901252c356
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-17 17:18:46 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,13 +14,13 @@ void print_number(int n);
 
 	if (i < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		i = -i;
 	}
 	if (i / 10)
 	{
 		print_number(i / 10);
 	}
-	putchar((i % 10) + '0');
+	_putchar((i % 10) + '0');
 
 }
