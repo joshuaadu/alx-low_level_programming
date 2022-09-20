7db358bccc013469e3a6b8c45acc55651f435167
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-20 03:05:43 -0700

    fix

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i, j, half;
+	char temp;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+		;
+	half = i / 2;
+
+	for (j = 0; j < half; j++)
+	{
+		temp = s[i];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = temp;
+	}
+
+	putchar('\n');
+}
