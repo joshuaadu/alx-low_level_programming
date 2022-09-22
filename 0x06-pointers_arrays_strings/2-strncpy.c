fdb47d3c5caec370f78d60fffbbea7059a7f8ca8
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-22 14:47:31 -0700

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,19 +14,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int len = 0;
 
-	while (i < n)
+	while (i < n && src[i])
 	{
-		if (src[i])
-			dest[i] = src[i];
-		else
-		{
-			dest[i] = '\0';
-			break;
-		}
+		dest[i] = src[i];
 		i++;
 	}
+	while (i < n)
+		dest[i++] = '\0';
 
 	return (dest);
 }
