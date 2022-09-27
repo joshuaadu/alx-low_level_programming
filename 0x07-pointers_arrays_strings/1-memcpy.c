759657015e6c3510a6997e011c51ff0eb0c731c0
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-27 08:12:54 -0700

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,10 +14,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	for (; i < n; i++, dest++, src++ )
-		dest = src;
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
 	return (dest);
 }
