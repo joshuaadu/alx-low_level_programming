a1fe1116d999a3909d52f7b4585411e75ca82a36
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-28 16:00:13 -0700

    add square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: number to find its square root
+ *
+ * Return: If n does not have a natural square root
+ * the function should return -1
+ *
+ */
+
+/**
+ * find_sqrt - find square root
+ * @p: number to find its square root
+ * @f: potential square root
+ * Return: root
+ *
+ */
+
+int find_sqrt(int p, int f)
+{
+	if (f * f == p)
+		return (f);
+	if (f * f > p)
+		return (-1);
+	return (find_sqrt(p, f + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	return (find_sqrt(n, 1));
+}
