7f0d378534d0a8731ede6c2007a5d5cfd881a29e
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-10-05 01:27:51 -0700

    fix

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,9 +12,9 @@
 
 void free_grid(int **grid, int height)
 {
-	if (grid != NULL && height != 0)
+	if (grid && height)
 	{
-		while (height-- >= 0)
+		while (--height >= 0)
 			free(grid[height]);
 		free(grid);
 	}
