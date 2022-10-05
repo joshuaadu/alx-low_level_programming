4ed1f23cfec9664b258b5681f3732f5df87bb385
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-10-04 22:53:01 -0700

    copy a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copy string to a newly allocated space in memory,
+ *
+ * @str: pointer to the string given as a parameter
+ *
+ * Return: a pointer to a newly allocated space in memory
+ * NULL if str = NULL
+ *
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0, j = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	/*printf("length of string: %d\n", i);*/
+
+	s = malloc(sizeof(char) * i);
+
+	if (s == NULL)
+		return (NULL);
+
+	for ( ; j < i; j++)
+		s[j] = str[j];
+
+	return (s);
+}
