94b8972cbb87afd6d5b3e2fbb3fbb79e871bead5
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-10-05 01:15:24 -0700

    free memory allocations in a 2 dimensional array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: pointer to a 2 dimensional grid
+ * @height: number of rows in grid
+ *
+ * Return: void
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	if (grid && height)
+	{
+		while (height-- >= 0)
+			free(grid[height]);
+		free(grid);
+	}
+}
