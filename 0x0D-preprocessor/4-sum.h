225649ff965eaa59b9ac916f1429041c5f2936b2
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-10-11 15:03:07 -0700

    computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_MACRO_H
+#define SUM_MACRO_H
+
+
+/**
+ * File: 4-sum.h
+ * Desc: computes the sum of the numbers x and y
+ * @x: number
+ * @y: number
+ */
+
+#define SUM(x, y) ((X) + (Y))
+#endif
