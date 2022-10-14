02dde0106901411f54be40c9d14cf3e3a02d7152
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-10-13 21:31:01 -0700

    prints strings, followed by a new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ *
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	char *str;
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if ((str = va_arg(list, char *)))
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (separator && i < (n - 1))
+			printf("%s", separator);
+	}
+
+	va_end(list);
+	printf("\n");
+}
