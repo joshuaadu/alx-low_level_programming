bfe04da666452d558b307869aa4d031335f9ea5b
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-10-20 21:14:57 +0000

    fix error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,17 @@ size_t print_list(const list_t *h)
 {
 	int count = 1;
 
+    if (!h)
+    {
+        return (0);
+    }
 	if (h->str)
 	{
 		printf("[%d] (%s)\n", h->len, h->str);
 	}
 	else
 	{
-		printf("[%d] (%s)\n", h->len, "nil");
+		printf("[0] (nil)\n");
 	}
 	if (h->next)
 	{
