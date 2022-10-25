9776133a20e88113f1badcaaa4d46e676e03b043
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-10-25 16:38:40 +0000

    get number of elements in a linked listint_t list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - get the number of elements in a linked listint_t list
+ * @h: head of the linked list
+ *
+ * Return: the number of nodes
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
