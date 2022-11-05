033e60c616d3c3cd08a069caa027f2892b07473b
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-11-05 20:05:07 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 
 /**
@@ -15,23 +16,18 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, n = 0, m;
+	int fd, len = 0, m;
 
 	if (!filename)
 		return (-1);
-	
-
+		
+	if (text_content)
+        len = strlen(text_content);
 	
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (text_content)
-    {
-        while (text_content[n++])
-            ;
-    }
-	
-    m = write(fd, text_content, n);
+    m = write(fd, text_content, len);
 
-	if (fd == -1 || m == -1 )
+	if (fd == -1 || m == -1)
 		return (-1);
 
 	
