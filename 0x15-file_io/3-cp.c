4fa18e34136e0793f118b66d55c0cf2a08a6fb61
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-11-08 15:53:04 +0000

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -57,24 +57,19 @@ void fdclose(int fd)
 int main(int argc, char *argv[])
 {
 	int from, to, r, w;
-	char *buf;
+	char buf[MAX];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buf = malloc(sizeof(char) * MAX);
-	if (!buf)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buf, MAX);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	do {
-		if (from == -1)
+		r = read(from, buf, MAX);
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 					"Error: Can't read from file %s\n", argv[1]);
@@ -88,10 +83,8 @@ int main(int argc, char *argv[])
 			free(buf);
 			exit(99);
 		}
-		r = read(from, buf, MAX);
-		to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
 	fdclose(from);
 	fdclose(to);
-	return (1);
+	return (0);
 }
